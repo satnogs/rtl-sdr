d7639bc3b738f997d35de3d25379b7d996bfa6a7
Alexander Schultze <alexschultze@gmail.com>
2015-03-18 01:15:19 +0100

    fix rtl_tcp memory corruption (can crash on small memory devices)

--- src/rtl_tcp.c
+++ src/rtl_tcp.c
@@ -236,10 +236,26 @@ void rtlsdr_callback(unsigned char *buf, uint32_t len, void *ctx)
 
 	if(!do_exit) {
 		struct llist *rpt = (struct llist*)malloc(sizeof(struct llist));
+		
+		fprintf(stderr, "*");
+
+		if(rpt==NULL){
+		fprintf(stderr, "WARNING: Could not malloc linked list! Package dropped.\n");
+		return;
+		}
+
 		rpt->data = (char*)malloc(len);
+
+		if(rpt->data ==NULL){
+		fprintf(stderr, "WARNING: Could not malloc ll buffer! Package dropped.\n");
+		free(rpt);
+		return;
+		}
+
 		memcpy(rpt->data, buf, len);
 		rpt->len = len;
 		rpt->next = NULL;
+		
 
 		pthread_mutex_lock(&ll_mutex);
 
